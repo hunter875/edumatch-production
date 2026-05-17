7585ebdffac505ed29175230b02b8729c6a6d495
hunter875 <minhln8a6@gmail.com>
2026-05-17 13:54:06 -0700

    fix nginx gateway internal container app routing

--- nginx-gateway/docker-entrypoint.d/99-render-main-conf.sh
+++ nginx-gateway/docker-entrypoint.d/99-render-main-conf.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -eu
 
-: "${AUTH_SERVICE_HOST:=auth-service}"
-: "${SCHOLARSHIP_SERVICE_HOST:=scholarship-service}"
-: "${CHAT_SERVICE_HOST:=chat-service}"
-: "${MATCHING_SERVICE_HOST:=matching-service}"
 : "${FRONTEND_HOST:=frontend-app}"
 
-envsubst '${AUTH_SERVICE_HOST} ${SCHOLARSHIP_SERVICE_HOST} ${CHAT_SERVICE_HOST} ${MATCHING_SERVICE_HOST} ${FRONTEND_HOST}' \
+envsubst '${FRONTEND_HOST}' \
   < /etc/nginx/main-templates/nginx.conf.template \
   > /etc/nginx/nginx.conf
